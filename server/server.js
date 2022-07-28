8a9317dc3d9e5a27af3392735ec588b02bc5161f
Erika Cannon <cannone.rva@gmail.com>
2022-07-28 17:07:08 -0400

    react files for pull

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
@@ -30,6 +32,16 @@ const startApolloServer = async (typeDefs, resolvers) => {
 await server.start();
 // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 
 db.once('open', () => {
     app.listen(PORT, () => {
