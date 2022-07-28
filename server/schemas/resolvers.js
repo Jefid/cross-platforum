698a9241565020a52593f3b46534aaa65c88adcf
Erika Cannon <cannone.rva@gmail.com>
2022-07-28 15:01:36 -0400

    finish friend mutation logic

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -92,74 +92,25 @@ const resolvers = {
       }
     
       throw new AuthenticationError('You need to be logged in!');
+    },
+
+    addFriend: async (parent, { friendId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { friends: friendId } },
+          { new: true }
+        ).populate('friends');
+    
+        return updatedUser;
+      }
+    
+      throw new AuthenticationError('You need to be logged in!');
     }
+
+  
   }
 
-  // Mutation: {
-  //   addUser: async (parent, args) => {
-  //     const user = await User.create(args);
-  //     const token = signToken(user);
-
-  //     return { token, user };
-  //   },
-  //   login: async (parent, { email, password }) => {
-  //     const user = await User.findOne({ email });
-
-  //     if (!user) {
-  //       throw new AuthenticationError('Incorrect credentials');
-  //     }
-
-  //     const correctPw = await user.isCorrectPassword(password);
-
-  //     if (!correctPw) {
-  //       throw new AuthenticationError('Incorrect credentials');
-  //     }
-
-  //     const token = signToken(user);
-  //     return { token, user };
-  //   },
-  //   addGamePost: async (parent, args, context) => {
-  //     if (context.user) {
-  //       const gamepost = await GamePost.create({ ...args, username: context.user.username });
-
-  //       await User.findByIdAndUpdate(
-  //         { _id: context.user._id },
-  //         { $push: { gameposts: gamepost._id } },
-  //         { new: true }
-  //       );
-
-  //       return gamepost;
-  //     }
-
-  //     throw new AuthenticationError('You need to be logged in!');
-  //   },
-  //   addComment: async (parent, { gamepostId, commentBody }, context) => {
-  //     if (context.user) {
-  //       const updatedGamePost = await GamePost.findOneAndUpdate(
-  //         { _id: gamepostId },
-  //         { $push: { comments: { commentBody, username: context.user.username } } },
-  //         { new: true, runValidators: true }
-  //       );
-
-  //       return updatedGamePost;
-  //     }
-
-  //     throw new AuthenticationError('You need to be logged in!');
-  //   },
-  //   addFriend: async (parent, { friendId }, context) => {
-  //     if (context.user) {
-  //       const updatedUser = await User.findOneAndUpdate(
-  //         { _id: context.user._id },
-  //         { $addToSet: { friends: friendId } },
-  //         { new: true }
-  //       ).populate('friends');
-
-  //       return updatedUser;
-  //     }
-
-  //     throw new AuthenticationError('You need to be logged in!');
-  //   }
-  // }
 };
 
 module.exports = resolvers;
