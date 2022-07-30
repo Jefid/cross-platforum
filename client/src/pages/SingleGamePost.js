703b50b5ba64d88a57e30a074e09d006869bc40a
Roger Vacovsky <rvacovsky@gmail.com>
2022-07-30 15:13:52 -0400

    added useQuery hook, friends component

--- client/src/pages/SingleGamePost.js
+++ client/src/pages/SingleGamePost.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_GAMEPOST } from '../utils/queries';
+import CommentList from '../components/CommentList';
+
 
 const SingleGamePost = props => {
+  const { id: gamepostId } = useParams();
+  
+  const { loading, data } = useQuery(QUERY_GAMEPOST, {
+    variables: { id: gamepostId }
+  });
+
+  const gamepost = data?.gamepost || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+
   return (
     <div>
       <div className="card mb-3">
         <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
-            Username
+           {gamepost.username}
           </span>{' '}
-          gamePlayed on createdAt
+          Posted on {gamepost.createdAt}
         </p>
         <div className="card-body">
-          <p>What game did you play?</p>
+          <p>{gamepost.gamePlayed}</p>
         </div>
       </div>
+
+      {gamepost.commentCount > 0 && (
+      <CommentList comments={gamepost.comments} />
+      )}
+
     </div>
   );
 };
