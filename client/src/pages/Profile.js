0e7c4c842a679e1c49705546fa1043d118be4268
ppepin34 <ppepin34@gmail.com>
2022-07-30 19:16:31 -0400

    front-end user authentication is done

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -9,7 +9,7 @@ import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 
 const Profile = (props) => {
-  
+
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
@@ -17,8 +17,6 @@ const Profile = (props) => {
   });
 
   const user = data?.me || data?.user || {};
-
-  console.log(user);
 
   // navigate to personal profile page if username is the logged-in user's
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
@@ -28,25 +26,32 @@ const Profile = (props) => {
   if (loading) {
     return <div>Loading...</div>;
   }
-  
-  
+
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+      </h4>
+    );
+  }
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {user.username}'s profile.
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
       </div>
 
       <div className="flex-row justify-space-between mb-3">
-          <div className="col-12 mb-3 col-lg-8">
-            <GamePostList
-              gameposts={user.gameposts}
-              title={`${user.username}'s games...`}
-            />
-          </div>
+        <div className="col-12 mb-3 col-lg-8">
+          <GamePostList
+            gameposts={user.gameposts}
+            title={`${user.username}'s games...`}
+          />
+        </div>
 
-      <div className="col-12 col-lg-3 mb-3">
+        <div className="col-12 col-lg-3 mb-3">
           <FriendList
             username={user.username}
             friendCount={user.friendCount}
