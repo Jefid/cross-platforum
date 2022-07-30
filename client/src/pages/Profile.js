2f67f3f7b560b9c2b9267f58090e2fe363511d39
ppepin34 <ppepin34@gmail.com>
2022-07-30 17:48:06 -0400

    encountering error with not getting userData on Home.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 
 import GamePostList from '../components/GamePostList';
 import FriendList from '../components/FriendList';
 
 import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import Auth from '../utils/auth';
+import auth from '../utils/auth';
 
 const Profile = (props) => {
   
   const { username: userParam } = useParams();
 
-  const { loading, data } = useQuery(QUERY_USER, {
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
   });
 
-  const user = data?.user || {};
+  const user = data?.me || data?.user || {};
+
+  // navigate to personal profile page if username is the logged-in user's
+  if (auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Navigate to="/profile" />;
+  }
 
   if (loading) {
     return <div>Loading...</div>;
