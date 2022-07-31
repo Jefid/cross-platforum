6cd963b549f5eb2bdc7d21f51812a9e81a88bec2
ppepin34 <ppepin34@gmail.com>
2022-07-30 20:17:02 -0400

    temp deletions in gamepost.js, added temp deletions .txt

--- server/models/GamePost.js
+++ server/models/GamePost.js
@@ -19,21 +19,6 @@ const gamePostSchema = new Schema(
             type: String,
             required: true
         },
-        rating: {
-            type: Number,
-            min: 0,
-            max: 5,
-        },
-        review: {
-            type: String,
-            minlength: 1,
-            maxlength: 280
-        },
-        platform: {
-            type: String,
-            minlength: 1,
-            maxlength: 20
-        },
         comments: [commentSchema]
     },
     {
