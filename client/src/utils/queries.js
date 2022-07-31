cfe702040ab5667552fc0ca2630c253afbd95ce9
Erika Cannon <cannone.rva@gmail.com>
2022-07-31 16:14:19 -0400

    commit before pull from main

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,17 +1,10 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_GAMEPOSTS = gql`
-<<<<<<< HEAD
-  query thoughts($username: String) {
-    gameposts(username: $username) {
-      _id
-      thoughtText
-=======
   query gameposts($username: String) {
     gameposts(username: $username) {
       _id
       gamePlayed
->>>>>>> 5ac4346d3994aa1f67d8cb4aea6cf86873afcf47
       createdAt
       username
       commentCount
