966f576214384d6d3fe2d25f52154b01484faf92
ppepin34 <ppepin34@gmail.com>
2022-07-31 12:21:54 -0400

    finish comment, and initial addGamePost

---
+++ client/src/components/CommentForm/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_COMMENT } from '../../utils/mutations';
+
+const CommentForm = ({ gamepostId }) => {
+    const [commentBody, setBody] = useState('');
+    const [characterCount, setCharacterCount] = useState(0);
+    const [addComment, { error }] = useMutation(ADD_COMMENT);
+
+    const handleChange = event => {
+        if (event.target.value.length <= 280) {
+            setBody(event.target.value);
+            setCharacterCount(event.target.value.length);
+        }
+    };
+
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+        try {
+            await addComment({
+                variable: { commentBody, gamepostId },
+            });
+            // clear form value
+        setBody('');
+        setCharacterCount(0);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+  return (
+    <div>
+      <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
+        Character Count: 0/280
+      </p>
+      <form className="flex-row justify-center justify-space-between-md align-stretch"
+      onSubmit={handleFormSubmit}
+      >
+        <textarea
+          placeholder="What's your take on this game?"
+          value={commentBody}
+          className="form-input col-12 col-md-9"
+          onChange={handleChange}
+        ></textarea>
+
+        <button className="btn col-12 col-md-3" type="submit">
+          Submit
+        </button>
+      </form>
+
+      {error && <div>Something went wrong...</div>}
+    </div>
+  );
+};
+
+export default CommentForm;
--- client/src/components/GamePostForm/index.js
+++ client/src/components/GamePostForm/index.js
@@ -1,9 +1,51 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_GAMEPOST } from '../../utils/mutations';
+import { QUERY_GAMEPOSTS, QUERY_ME } from '../../utils/queries';
 
 const GamePostForm = () => {
   
     const [gamePlayed, setText] = useState('');
     const [characterCount, setCharacterCount] = useState(0);
+    const [addGamePost, { error }] = useMutation(ADD_GAMEPOST, {
+      update(cache, { data: { addGamePost } }) {
+        // could potentially no exist yet, so wrap in a try/catch statement
+        try{
+          // update me array's cache
+          const { me } = cache.readQuery({ query: QUERY_ME }) ;
+          cache.writeQuery({
+            query: QUERY_ME,
+            data: { me: { ...me, gameposts: [...me.gameposts, addGamePost] } },
+          });
+        } catch (e) {
+          console.warn("First thought insertion by user")
+        }
+
+        // update gamepost array's cache
+        const { gameposts } = cache.readQuery({ query: QUERY_GAMEPOSTS });
+        cache.writeQuery({
+          query: QUERY_GAMEPOSTS,
+          data: { gameposts: [addGamePost, ...gameposts] },
+        });
+      }
+    });
+
+    const handleFormSubmit = async event => {
+      event.preventDefault();
+
+      try{
+        // add gamePost to database
+        await addGamePost({
+          variables: { gamePlayed }
+        });
+
+        // clear form value
+        setText('');
+        setCharacterCount(0);
+      } catch (e) {
+        console.error(e);
+      }
+    };
   
     const handleChange = event => {
         if (event.target.value.length <= 280) {
@@ -16,8 +58,10 @@ const GamePostForm = () => {
     <div>
       <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
         Character Count: {characterCount}/280
+        {error && <span className="ml-2">Something went wrong...</span>}
       </p>
-      <form className="flex-row justify-center justify-space-between-md align-stretch">
+      <form className="flex-row justify-center justify-space-between-md align-stretch"
+      onSubmit={handleFormSubmit}>
       <textarea
             placeholder="Tell us about a new game you played..."
             value={gamePlayed}
